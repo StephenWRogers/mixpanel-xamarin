31c93c5669be117d6505c0eacf0794a089c7290d
Dmitry Egorov <degorov86@gmail.com>
2015-04-20 22:20:26 +0300

    * removed extra frameworks from binding project * added relative path for static lib reference in mixpanel project

--- BindingProject/libMixpanel.linkwith.cs
+++ BindingProject/libMixpanel.linkwith.cs
@@ -1,11 +1,8 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith (
-    "libMixpanel.a",
-    LinkTarget = LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
-    Frameworks = "MapKit ImageIO Accelerate Security CFNetwork AdSupport CoreGraphics CoreTelephony Foundation QuartzCore SystemConfiguration UIKit",
-    LinkerFlags = "-ObjC -licucore",
-    SmartLink = true,
-    ForceLoad = true
-)]
+[assembly: LinkWith("libMixpanel.a",
+    LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator | LinkTarget.Simulator64,
+	SmartLink = true, ForceLoad = true,
+	Frameworks = "UIKit Foundation SystemConfiguration CoreTelephony Accelerate CoreGraphics QuartzCore",
+    LinkerFlags="-ObjC -licucore")]
